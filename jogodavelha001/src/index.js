da7e6da00180b7bb096e2c6589491c5be8e54ce4
DGloving <117310650+DGloving@users.noreply.github.com>
2023-12-10 09:32:18 -0300

    Começamos a nossa arquitetura de pastas, usando a ITCSS

--- jogodavelha001/src/index.js
+++ jogodavelha001/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/settings/colors.css';
 import './styles/generic/reset.css';
-import './index.css';
+import './styles/elements/base.css';
 import App from './App';
 
 
