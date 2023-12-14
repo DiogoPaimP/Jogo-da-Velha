193823cec250e62102065b53a34fa0ed45b35b43
DGloving <117310650+DGloving@users.noreply.github.com>
2023-12-14 12:34:19 -0300

    criamos o object da nossa Logo

--- jogodavelha001/src/objects/Logo/index.js
+++ jogodavelha001/src/objects/Logo/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import Life from '../../img/logo.png';
 
-const Logo = () => (<img src="/img/logo.png" alt="Logo da tag Life" />);
+const Logo = () => (<img src={Life} alt="Logo da tag Life" />);
 
 export default Logo;
