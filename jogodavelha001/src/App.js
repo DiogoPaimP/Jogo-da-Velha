28935945a252015b9f56026965231a0cec67930c
DGloving <117310650+DGloving@users.noreply.github.com>
2023-12-14 12:49:42 -0300

    refazendo o objecti da nossa LOGO

--- jogodavelha001/src/App.js
+++ jogodavelha001/src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import Sobre from './objects/Sobre';
-import Logo from './objects/logo';
-
 
 function App() {
   return (
-    <div>
-      <Logo />
       <Sobre />
-      
-    </div>
   );
 }
 
--- jogodavelha001/src/objects/Logo/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Life from '../../img/logo.png';
-
-const Logo = () => (<img src={Life} alt="Logo da tag Life" />);
-
-export default Logo;
