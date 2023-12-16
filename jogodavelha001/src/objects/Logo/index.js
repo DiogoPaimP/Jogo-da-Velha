040ffc7746d4fe2b8369852a585dc92414b0eb05
DGloving <117310650+DGloving@users.noreply.github.com>
2023-12-16 10:34:39 -0300

    trocamos a nossa logo, novamente
    nossa logo, ao menos por enquanto

--- jogodavelha001/src/objects/Logo/index.js
+++ jogodavelha001/src/objects/Logo/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Styles from './estilo.css'
 import Life from '../../img/logo.png';
 
-const Logo = () => (<img src={Life} alt="Logo da Life" />);
+const Logo = () => (<img id="imagem_redoda" src={Life} alt="Logo da Life" />);
 
 export default Logo;
