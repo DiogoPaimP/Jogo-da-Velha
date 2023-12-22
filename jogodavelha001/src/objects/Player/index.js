33623ef02e15979074c8f8d9ad5c812724da8aa8
DGloving <117310650+DGloving@users.noreply.github.com>
2023-12-22 11:21:36 -0300

    adicionamos o nosso player X e as imagens do player X e O

--- jogodavelha001/src/objects/Player/index.js
+++ jogodavelha001/src/objects/Player/index.js
@@ -1,8 +1,11 @@
 import React from "react";
-
+import './styles.css';
+import playerX from '../../img/X.png'
 
 const Player = () => (
-    <button className="player">X</button>
+    <button className="player">
+        <img src={playerX} alt="Jogador X"></img>
+    </button>
 );
 
 export default Player;
