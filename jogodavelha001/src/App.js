5f50a9a104fcd21b77409f67864bbfacd9736954
DGloving <117310650+DGloving@users.noreply.github.com>
2024-01-03 11:00:08 -0300

    Criamos o Object Input

--- jogodavelha001/src/App.js
+++ jogodavelha001/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import './App.css';
 
+import Input from './objects/Input';
 import Label from './objects/Label';
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
+
 
 
 const App = () =>  (
   <main className='app'>
     <Header />
     <Hashtag />
+    <Input value="value"/>
     <Label content="Mostrar eventos" /> 
   </main>
   );
---
+++ jogodavelha001/src/objects/Input/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const Input = ({value}) => (
+    <input type="checkbox" value= {value}/>
+);
+
+
+export default Input;
