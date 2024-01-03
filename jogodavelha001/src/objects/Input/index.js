39cbe597914334c7c0b9857a21f891e37ba834fd
DGloving <117310650+DGloving@users.noreply.github.com>
2024-01-03 11:14:13 -0300

    Associamos o nosso Label com o nosso Input

--- jogodavelha001/src/objects/Input/index.js
+++ jogodavelha001/src/objects/Input/index.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Label from "../Label";
 
+const Input = ({id ,value, content}) => (
+    <>
+        <input id={id} type="checkbox" value= {value}/>
+        <Label htmlFor="show" content={content} /> 
+    </>
 
-const Input = ({value}) => (
-    <input type="checkbox" value= {value}/>
 );
 
 
